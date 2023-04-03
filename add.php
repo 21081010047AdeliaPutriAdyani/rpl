78518e6abf00c06f00607cbbb2db79835b89114d
21081010047AdeliaPutriAdyani <21081010047@student.upnjatim.ac.id>
2023-04-03 13:00:00 +0700

    br

--- add.php
+++ add.php
@@ -9,18 +9,17 @@
         <img src="login.png" height="100" width="100" style="display:block; margin:auto;"/>
     </p>
     <form action="add.php" method="post" name="form1">
-        <table style="margin-left: auto; margin-right: auto; width=25%; border=0;">
+        <table style="margin:auto; width=25%; border=0;">
             <tr> <td>USERNAME</td> </tr>
             <tr> <td><input type="text" name="username"></td> </tr>
             
             <tr> <td>PASSWORD</td> </tr>
             <tr> <td><input type="text" name="password"></td> </tr>
 
-            <tr> <td>Lupa Password?</td> </tr>
             <tr><td><input type="submit" name="Submit" value="ENTER"></td> </tr>
 
             <tr> <td>Belum Punya Akun?</td> </tr>
-            <tr> <td>Sign Up</td> </tr>
+            <tr> <td> <a href="signup.php"> SIGN UP </a> </td> </tr>
         </table>
     </form>
 
@@ -33,9 +32,9 @@
 
         include("koneksi.php");
 
-        $hasil = mysql_query("INSERT INTO employees(username,password) VALUES('$username','$password')");
+        $hasil = mysql_query("INSERT INTO employees(username, password) VALUES('$username','$password')");
 
-        echo "User added successfully. <a href='signup.php'>View Users</a>";
+        echo "User added successfully. <a href='admin.php'>View Users</a>";
     }
     ?>
 </body>
