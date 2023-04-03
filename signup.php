0bed26dba3d7e5bda4da51dc7e08c300c7084431
21081010047AdeliaPutriAdyani <21081010047@student.upnjatim.ac.id>
2023-04-03 14:18:38 +0700

    w

--- signup.php
+++ signup.php
@@ -4,13 +4,13 @@
     $status = '';
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $id_employees = $_POST['id_employees'];
+        $id_karyawan = $_POST['id_karyawan'];
         $nama_karyawan = $_POST['nama_karyawan'];
         $username = $_POST['username'];
         $password = $_POST['password'];
         $jabatan = $_POST['jabatan'];
 
-        $query = "INSERT INTO employees(id_employees,nama_karyawan,username,password,jabatan) VALUES('$id_employees','$nama_karyawan','$username','$password','$jabatan')";
+        $query = "INSERT INTO employees (id_employees,nama_karyawan,username,password,jabatan) VALUES('$id_karyawan','$nama_karyawan','$username','$password','$jabatan')";
         
         $result = mysqli_query(connection(),$query);
         if ($result) {
@@ -31,42 +31,42 @@
     <p>
         <img src="login.png" height="100" width="100" style="display:block; margin:auto;"/>
     </p>
-    <form action="add.php" method="post" name="form1">
+    <form action="signup.php" method="POST">
         <table style="margin-left: auto; margin-right: auto; width=25%; border=0;">
             <tr> 
                 <td>Id Karyawan</td>
-                <td><input type="text" name="username"></td> 
+                <td><input type="text" name="id_karyawan" required="required"></td> 
             </tr>
             
             <tr> 
                 <td>Nama Karyawan</td>
-                <td><input type="text" name="password"></td> 
+                <td><input type="text" name="nama_karyawan" required="required"></td> 
             </tr>
             
             <tr> 
                 <td>Username</td>
-                <td><input type="text" name="password"></td> 
+                <td><input type="text" name="username" required="required"></td> 
             </tr>
 
             <tr> 
                 <td>Password</td>
-                <td><input type="text" name="username"></td> 
+                <td><input type="text" name="password" required="required"></td> 
             </tr>
 
             <tr> 
                 <td>Jabatan</td>
-                <td><input type="text" name="username"></td> 
+                <td><input type="text" name="jabatan" required="required"></td> 
             </tr>
 
             <tr> 
-                <td><input type="submit" name="GET STARTED" value="GET STARTED"></td> 
+                <td><input type="submit" name="submit" value="GET STARTED"></td> 
             </tr>
             <?php 
                           if ($status=='ok') {
-                            echo '<br><br><div class="alert alert-success" role="alert">Data Customer berhasil disimpan</div>';
+                            echo '<br><br><div class="alert alert-success" role="alert">Data Karyawan berhasil disimpan</div>';
                           }
                           elseif($status=='err'){
-                            echo '<br><br><div class="alert alert-danger" role="alert">Data Customer gagal disimpan</div>';
+                            echo '<br><br><div class="alert alert-danger" role="alert">Data Karyawan gagal disimpan</div>';
                           }
                         ?>
         </table>
